0b10343ec6d4901b218adfb96142175aa4bffae9
流浪龟7 <thankamt@163.com>
2021-06-03 14:41:51 +0800

    添加LeetCode50题解法

---
+++ src/com/leetcode/item50/PowSum.java
@@ -0,0 +1,23 @@
+package com.leetcode.item50;
+
+public class PowSum {
+
+
+    public double myPow(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+
+    public double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+
+    public static void main(String[] args) {
+        PowSum ps = new PowSum();
+        System.out.println(ps.myPow(2,10));
+    }
+}
