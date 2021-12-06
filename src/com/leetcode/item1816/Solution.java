289c9f7f18ee70f1572035319070af85a99ce8c0
流浪龟7 <thankamt@163.com>
2021-12-06 16:39:33 +0800

    1816题解决

---
+++ src/com/leetcode/item1816/Solution.java
@@ -0,0 +1,69 @@
+package com.leetcode.item1816;
+
+/**
+ * 句子 是一个单词列表，列表中的单词之间用单个空格隔开，且不存在前导或尾随空格。每个单词仅由大小写英文字母组成（不含标点符号）。
+ * <p>
+ * 例如，"Hello World"、"HELLO" 和 "hello world hello world" 都是句子。
+ * 给你一个句子 s 和一个整数 k ，请你将 s 截断 ，使截断后的句子仅含 前 k 个单词。返回 截断 s 后得到的句子。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "Hello how are you Contestant", k = 4
+ * 输出："Hello how are you"
+ * 解释：
+ * s 中的单词为 ["Hello", "how" "are", "you", "Contestant"]
+ * 前 4 个单词为 ["Hello", "how", "are", "you"]
+ * 因此，应当返回 "Hello how are you"
+ * 示例 2：
+ * <p>
+ * 输入：s = "What is the solution to this problem", k = 4
+ * 输出："What is the solution"
+ * 解释：
+ * s 中的单词为 ["What", "is" "the", "solution", "to", "this", "problem"]
+ * 前 4 个单词为 ["What", "is", "the", "solution"]
+ * 因此，应当返回 "What is the solution"
+ * 示例 3：
+ * <p>
+ * 输入：s = "chopper is not a tanuki", k = 5
+ * 输出："chopper is not a tanuki"
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 500
+ * k 的取值范围是 [1, s 中单词的数目]
+ * s 仅由大小写英文字母和空格组成
+ * s 中的单词之间由单个空格隔开
+ * 不存在前导或尾随空格
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        String s ="chopper is not a tanuki";
+        int k=5;
+        System.out.println(truncateSentence2(s,k));
+        System.out.println(truncateSentence(s,k));
+    }
+
+    public static String truncateSentence(String s, int k) {
+        String[] sArr = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<k;i++){
+            sb.append(sArr[i]).append(" ");
+        }
+        return sb.delete(sb.length()-1,sb.length()).toString();
+    }
+
+    public static String truncateSentence2(String s, int k) {
+
+        int index=0;
+        for(int i=0;i<k;i++){
+            index = s.indexOf(" ",index+1);
+        }
+        if(index==-1){
+            index = s.length();
+        }
+        return s.substring(0,index);
+    }
+
+}
