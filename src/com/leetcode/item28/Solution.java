f199fc5c54444f84f06cec8a905698fdeefa2e4a
流浪龟7 <thankamt@163.com>
2022-01-14 16:24:47 +0800

    28题未解答

---
+++ src/com/leetcode/item28/Solution.java
@@ -0,0 +1,29 @@
+package com.leetcode.item28;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(strStr("mississippi", "issip"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+//        int indexNeed = 0;
+//        for (int i = 0; i < haystack.length(); i++) {
+//            if (indexNeed == needle.length()) {
+//                return i - needle.length();
+//            }
+//            if (haystack.charAt(i) == needle.charAt(indexNeed)) {
+//                indexNeed++;
+//            } else {
+//                indexNeed = 0;
+//            }
+//        }
+//        if (indexNeed != 0) {
+//            return haystack.length() - indexNeed;
+//        }
+        return -1;
+    }
+}
