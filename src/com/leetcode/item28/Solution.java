9e9a5c4b9c134fc200884741a1927336747c55b0
流浪龟7 <thankamt@163.com>
2022-01-18 15:17:04 +0800

    修改28题代码

--- src/com/leetcode/item28/Solution.java
+++ src/com/leetcode/item28/Solution.java
@@ -6,25 +6,35 @@ public class Solution {
         System.out.println(strStr("mississippi", "issip"));
     }
 
+    /**
+     * 我自己写的
+     * @param haystack
+     * @param needle
+     * @return
+     */
     public static int strStr(String haystack, String needle) {
         if (needle.length() == 0) {
             return 0;
         }
 
-//        int indexNeed = 0;
-//        for (int i = 0; i < haystack.length(); i++) {
-//            if (indexNeed == needle.length()) {
-//                return i - needle.length();
-//            }
-//            if (haystack.charAt(i) == needle.charAt(indexNeed)) {
-//                indexNeed++;
-//            } else {
-//                indexNeed = 0;
-//            }
-//        }
-//        if (indexNeed != 0) {
-//            return haystack.length() - indexNeed;
-//        }
+        int indexNeed = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            if (indexNeed == needle.length()) {
+                return i - needle.length();
+            }
+            if (haystack.charAt(i) == needle.charAt(indexNeed)) {
+                indexNeed++;
+            } else {
+                indexNeed = 0;
+            }
+        }
+        if (indexNeed != 0) {
+            return haystack.length() - indexNeed;
+        }
+        return -1;
+    }
+
+    public static int strStr2(String haystack, String needle) {
         return -1;
     }
 }
