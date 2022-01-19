e99b2348ac4ab2912d32d8d8442540a8bb4f2d47
流浪龟7 <thankamt@163.com>
2022-01-19 09:36:32 +0800

    539题暂时提交

---
+++ src/com/leetcode/item539/Solution.java
@@ -0,0 +1,22 @@
+package com.leetcode.item539;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        List<String> strs = Arrays.asList("00:00","23:59","00:00");
+        Collections.sort(strs);
+        System.out.println(strs);
+
+    }
+
+    public static int findMinDifference(List<String> timePoints) {
+
+
+        return 0;
+    }
+}
